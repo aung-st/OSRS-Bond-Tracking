54816d706a15ae404534a03d675ea5219db04212
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-10-23 23:29:26 +0100

    Add: functionality to create id function

--- src/dump_json.py
+++ src/dump_json.py
@@ -1,10 +1,28 @@
 import json
+import hashlib
+import os 
 
 def dump_json():
     pass
 
-def create_filename():
+def create_filename() -> str:
     pass
 
-def create_id():
-    pass
+def create_id(length:str) -> str:
+    
+    """
+    Create a hash id by encoding a string of random bytes of size length.
+
+    Parameters:
+    length (int): Size of random bytes
+
+    Returns:
+    A hash of length 40
+    """
+
+    # generate a hash object for a randomly generated string
+    hash_object = hashlib.sha1()
+    hash_object.update(str(os.urandom(length)).encode())
+    hash_id = hash_object.hexdigest()
+
+    return hash_id
