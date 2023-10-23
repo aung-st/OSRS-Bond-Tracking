304629533b52f1e635789e3880b8e1fa27cdc656
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-10-24 00:20:23 +0100

    Add: unit tests to ensure correct dictionaries are fetched from each API endpoint

--- test/test_api_call.py
+++ test/test_api_call.py
@@ -7,3 +7,9 @@ def test_data_details_is_fetched():
 
 def test_data_graph_is_fetched():
     assert isinstance(get_data_graph(),dict) 
+
+def test_data_details_is_correct():
+    assert len(get_data_details()) == 1
+
+def test_data_graph_is_correct():
+    assert len(get_data_graph()) == 2
