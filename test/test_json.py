7302ef0e7d7091ab7e796a6b3f79c8e0aea0e7a3
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-10-23 23:33:37 +0100

    Add: test to ensure id is created successfully and comments for detail json test

--- test/test_json.py
+++ test/test_json.py
@@ -1,14 +1,23 @@
-from src.dump_json import dump_json, create_filename 
+from src.dump_json import dump_json, create_filename, create_id
 from src.get_data import get_data_details, get_data_graph
 import os 
 
-def test_raw_json_is_dumped():
+def test_id_is_created():
+
+    # generate random id
+    id = create_id(3)
+    assert isinstance(id,str) and len(id) == 40
+
+def test_detail_json_is_dumped():
+
+    # test file path for json details
     json_path = "data/test_json_dump/details"
     raw_json = get_data_details()
 
-    # dump json file
+    # create filename and fetch id 
     filename,id = create_filename(json_path)
 
+    # dump json file
     dump_json(raw_json,filename)
 
     filepath = os.listdir(json_path)
