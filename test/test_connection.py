875974f1c996b428e17e76412566ffc5f27017d6
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-10-29 16:56:11 +0000

    Add: test connection module to test database connections

---
+++ test/test_connection.py
@@ -0,0 +1,20 @@
+from mysql.connector import connect
+from src.database_utils import create_database
+from os import getenv
+
+# Ensure that database connections can be established 
+def test_connection_works():
+
+    create_database("test_osrs_db")
+
+    connection = connect(
+        host='localhost',
+        database='test_osrs_db',
+        user="root",
+        password=getenv("mysql_root")
+        )
+    
+    assert connection.is_connected()
+            
+
+    
