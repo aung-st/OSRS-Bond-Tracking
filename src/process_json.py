d356e26f4e44717b1ea2acf22e54b64766272f84
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-11-15 15:13:09 +0000

    Add: create graph key to generate a primary key for the graphs table

--- src/process_json.py
+++ src/process_json.py
@@ -20,3 +20,20 @@ def create_detail_key(
     
     return id+'-'+time_extracted
 
+def create_graph_key(
+    id:str,
+    timestamp:str,
+) -> str:
+    """
+    Generate an n-character hash key + a timestamp in milliseconds seperated by a '-' character to be used as primary key
+    for the graphs table.
+
+    Parameters:
+    id (str): Hash id of json file
+    timestamp (str): timestamp in milliseconds since January 1st 1970
+
+    Returns:
+    id+'-'+ timestamp (str): A string concatenation to be used as a primary key in a database
+    """
+    
+    return id+'-'+timestamp
