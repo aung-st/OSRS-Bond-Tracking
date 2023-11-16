ce5b2883c7cfc561a2b263a93f52624cc2b8e3f5
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-11-16 23:17:13 +0000

    Add: unit test to ensure main database connection is also successful

--- test/test_connection.py
+++ test/test_connection.py
@@ -3,7 +3,7 @@ from src.database_utils import create_database
 from os import getenv
 
 # Ensure that database connections can be established 
-def test_connection_works():
+def test_sample_db_connection_works():
 
     create_database("test_osrs_db")
 
@@ -15,6 +15,20 @@ def test_connection_works():
         )
     
     assert connection.is_connected()
+
+def test_main_db_connection_works():
+
+    create_database("osrs_db")
+
+    connection = connect(
+        host='localhost',
+        database='test_osrs_db',
+        user="root",
+        password=getenv("mysql_root")
+        )
+    
+    assert connection.is_connected()
+            
             
 
     
