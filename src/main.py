01751e0de9ed7e0c1a90c14aef1761ae748ffe0f
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-11-22 14:44:58 +0000

    Add: driver code for desktop notifications

---
+++ src/main.py
@@ -0,0 +1,47 @@
+from get_data import get_data_five_minute_average
+from dump_json import create_filename,dump_json
+from process_json import bulk_process_five_minute_average_json
+from desktop_notifications import notify
+import time
+
+
+
+if __name__ == "__main__":
+
+    # how long the program runs for in seconds
+    time_end = time.time() + 20
+    
+    while time.time() < time_end:
+        
+        # call five minute average data on old school bond (item id: 13190)
+        five_minute_average_raw_json = get_data_five_minute_average()['data']['13190']
+
+        # json path
+        five_minute_average_json_path = "data/json_dump/five_minute_averages/"
+
+        # hash id length
+        id_length = 4
+
+        # create filename and fetch id
+        five_minute_averages_filename,five_minute_averages_id = create_filename(five_minute_average_json_path,id_length)
+
+        # dump five minute average json file
+        dump_json(five_minute_average_raw_json,five_minute_averages_filename)
+        
+        # user defined threshold
+        threshold = 9500000
+
+        # add data to database
+        bulk_process_five_minute_average_json('osrs_db',five_minute_average_raw_json,five_minute_averages_id)
+
+        # give notification if price is under threshold 
+        if five_minute_average_raw_json['avgHighPrice'] < threshold or five_minute_average_raw_json['avgLowPrice'] < threshold:
+            notify(five_minute_average_raw_json,threshold)
+        
+        # pause execution to avoid spamming API calls
+        time.sleep(5)
+
+
+
+    
+
